bef61fb5644cf2b24305333603bced65fe965c58
Jono Brandel <developer@jono.fyi>
2025-01-19 22:00:24 -0800

    Able to recursively pass context

--- lib/Context.ts
+++ lib/Context.ts
@@ -1,5 +1,9 @@
-import React, { createContext, useContext } from 'react';
+import { createContext, useContext } from 'react';
 import Two from 'two.js';
+import { Element } from 'two.js/src/element';
 
-export const Context = createContext<React.MutableRefObject<Two | null> | null>(null);
+export const Context = createContext<{
+  two: Two | null;
+  parent: Element | null;
+}>({ two: null, parent: null });
 export const useTwo = () => useContext(Context);
--- lib/Provider.tsx
+++ lib/Provider.tsx
@@ -1,33 +1,58 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import Two from 'two.js';
-import { Context } from './Context';
+import { Context, useTwo } from './Context';
 
 type TwoConstructorProps = ConstructorParameters<typeof Two>[0];
 type TwoConstructorPropsKeys = NonNullable<TwoConstructorProps>;
 type ComponentProps = React.PropsWithChildren<TwoConstructorPropsKeys>;
 
 export const Provider: React.FC<ComponentProps> = (props) => {
+  const { two, parent } = useTwo();
   const container = useRef<HTMLDivElement | null>(null);
-  const ref = useRef<Two | null>(null);
+  const [state, set] = useState<{ two: typeof two; parent: typeof parent }>({
+    two,
+    parent,
+  });
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(mount, []);
+  useEffect(update, [props]);
 
   function mount() {
-    const two = (ref.current = new Two(props).appendTo(container.current!));
+    let unmount = () => {};
+    const isRoot = !two;
+
+    if (isRoot) {
+      const two = new Two(props).appendTo(container.current!);
+      set({ two, parent: two.scene });
+      unmount = () => {
+        // TODO: Release memory
+        const index = Two.Instances.indexOf(two);
+        Two.Instances.splice(index, 1);
+        two.pause();
+      };
+    }
 
     return unmount;
+  }
 
-    function unmount() {
-      // TODO: Release memory
-      const index = Two.Instances.indexOf(two);
-      Two.Instances.splice(index, 1);
-      two.pause();
-    }
+  function update() {
+    set(({ two, parent }) => {
+      if (two) {
+        for (const key in props) {
+          if (key in two) {
+            // TODO: Test
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            (two as any)[key] = (props as any)[key];
+          }
+        }
+      }
+      return { two, parent };
+    });
   }
 
   return (
-    <Context.Provider value={ref}>
+    <Context.Provider value={state}>
       <div ref={container}>{props.children}</div>
     </Context.Provider>
   );
