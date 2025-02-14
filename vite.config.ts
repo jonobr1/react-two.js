45d7c7af87427369c13c3edb16d2b5ad8f5bea28
Jono Brandel <developer@jono.fyi>
2025-02-13 22:16:14 -0800

    Rename to @react/two.js

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: {
-      name: '@react-two/fiber',
+      name: '@react/two.js',
       entry: resolve(__dirname, 'lib/main.ts'),
       fileName: (format, entryName) => `react-two-${entryName}.${format}.js`,
       formats: ['es'],
